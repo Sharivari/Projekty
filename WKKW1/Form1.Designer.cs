d8f4e90bb3f453f8ccef30845465032bb7377ed1
Sharivari <koralgalaxis@gmail.com>
2017-07-13 17:50:24 +0200

    Algorytm genetyczny w zawodach WKKW

---
+++ WKKW1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WKKW1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(200, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(500, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(93, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Population";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(386, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Max iterations";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Max = 100";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(543, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Max = 200";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(673, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 42);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Ilosc wykonanych iteracji: ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Najlepszy osobnik: ";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.Title = "Iteracje";
+            chartArea1.AxisY.Title = "FC";
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.BackColor = System.Drawing.Color.Transparent;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(500, 248);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(353, 239);
+            this.chart1.TabIndex = 9;
+            this.chart1.Text = "chart1";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "horse30.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 551);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Algorytm genetyczny w WKKW";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ WKKW1/Form1.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+// Allow managed code to call unmanaged functions that are implemented in a DLL
+using System.Runtime.InteropServices;
+
+namespace WKKW1
+{
+
+
+    public partial class Form1 : Form
+    {
+        int InputIteration = 0;
+        int InputPopulation = 0;
+        
+
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void CreateNew(int Input);
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void HorseTest();
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SaveBest();
+        [DllImport("WKKW.dll", CharSet = CharSet.Ansi)]
+        public static extern IntPtr BinGenerator();
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int getBest();
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int getPopulationSize();
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern float getPopulationFC(int i);
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void MinMaxSelector(float GFC);
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void NewGenerationSelector();
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void BreedSelector();
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ParentSelector();
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void CrossBreed();
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ApplyPopulation();
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void FindBest();
+
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int getJumpPoints(int i);
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int getCrossPoints(int i);
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern float getDressagePoints(int i);
+
+
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static extern bool Chance(double Percent);
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static extern bool ifStop(bool Iteration, int It, float Best, float Argument);
+        [DllImport("WKKW.dll", CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static extern bool ifStop2Arguments(int ActualIteration, int MaxIteration, float Best, float MaxBest);
+
+        public Form1()
+        {
+            InitializeComponent();
+            //this.BackColor = Color.White;
+        }
+
+       
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_Validated(object sender, EventArgs e)
+        {
+
+
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var input1 = textBox1.Text;
+            var input2 = textBox2.Text;
+
+            int integer;
+            if (Int32.TryParse(textBox1.Text, out integer) == true)
+            {
+
+                if (textBox1.Text != null && !string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    InputPopulation = Int32.Parse(input1);
+
+                    if (InputPopulation > 100)
+                    {
+                        InputPopulation = 100;
+                        textBox1.Text = "100";
+                        textBox1.Invalidate();
+                    }
+                    if (InputPopulation <= 0)
+                    {
+                        InputPopulation = 2;
+                        textBox1.Text = "2";
+                        textBox1.Invalidate();
+                    }
+                }
+                if (!string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+                {
+                    button1.Enabled = true;
+                }
+                else button1.Enabled = false;
+
+            }
+            else textBox1.Text = "";
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                button1.Enabled = true;
+            }
+            else button1.Enabled = false;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            var input1 = textBox1.Text;
+            var input2 = textBox2.Text;
+
+            int integer;
+            if (Int32.TryParse(textBox1.Text, out integer) == true)
+            {
+                if (textBox2.Text != null && !string.IsNullOrWhiteSpace(textBox2.Text))
+                {
+                    InputIteration = Int32.Parse(input2);
+                    if (InputIteration > 200)
+                    {
+                        InputIteration = 200;
+                        textBox2.Text = "200";
+                        textBox2.Invalidate();
+                    }
+                    if (InputIteration <= 0)
+                    {
+                        InputIteration = 2;
+                        textBox2.Text = "2";
+                        textBox2.Invalidate();
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+                {
+                    button1.Enabled = true;
+                }
+                else button1.Enabled = false;
+            }
+            else textBox1.Text = "";
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                button1.Enabled = true;
+            }
+            else button1.Enabled = false;
+        }
+
+        private void textBox2_Validated(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            
+
+            while (chart1.Series.Count > 0) { chart1.Series.RemoveAt(0); }
+            List<float> Bests = new List<float>();
+            //PictureBox[] pictureBoxs = new PictureBox[InputPopulation];
+            List<PictureBox> lst = new List<PictureBox>();
+            
+
+            float GlobalFC; //Globalne FC
+            int n = 1; //Iteracja - zaczynamy od pierwszej
+            bool Iteration; //Do warunków stopu
+            bool stop = true;
+            float StopArgument;
+            int Key;
+            stop = true;
+            Random random = new Random();
+
+            CreateNew(InputPopulation); //Generowanie nowej populacji - wejściowej
+            label5.Text = getPopulationSize().ToString();
+
+            
+
+            Bitmap baseImage;
+            Bitmap overlayImage;
+            Bitmap overlayImage0;
+            Bitmap overlayImage1;
+            Bitmap overlayImage2;
+            Bitmap overlayImage3;
+            Bitmap overlayImage4;
+            Bitmap overlayImage5;
+            Bitmap overlayImage6;
+            Bitmap overlayImage7;
+            Bitmap overlayImage8;
+            Bitmap overlayImage9;
+            Bitmap overlayImage10;
+
+            baseImage = (Bitmap)Image.FromFile("WKKWimages\\skoki500.png");
+            baseImage.SetResolution(110,110);
+
+            overlayImage = (Bitmap)Image.FromFile(@"WKKWimages\\horse30.png");
+            overlayImage.SetResolution(100, 100);
+            overlayImage0 = (Bitmap)Image.FromFile("WKKWimages\\horse0.png");
+            overlayImage0.SetResolution(100, 100);
+            overlayImage1 = (Bitmap)Image.FromFile("WKKWimages\\horse1.png");
+            overlayImage1.SetResolution(100, 100);
+            overlayImage2 = (Bitmap)Image.FromFile("WKKWimages\\horse2.png");
+            overlayImage2.SetResolution(100, 100);
+            overlayImage3 = (Bitmap)Image.FromFile("WKKWimages\\horse3.png");
+            overlayImage3.SetResolution(100, 100);
+            overlayImage4 = (Bitmap)Image.FromFile("WKKWimages\\horse4.png");
+            overlayImage4.SetResolution(100, 100);
+            overlayImage5 = (Bitmap)Image.FromFile("WKKWimages\\horse5.png");
+            overlayImage5.SetResolution(100, 100);
+            overlayImage6 = (Bitmap)Image.FromFile("WKKWimages\\horse6.png");
+            overlayImage6.SetResolution(100, 100);
+            overlayImage7 = (Bitmap)Image.FromFile("WKKWimages\\horse7.png");
+            overlayImage7.SetResolution(100, 100);
+            overlayImage8 = (Bitmap)Image.FromFile("WKKWimages\\horse8.png");
+            overlayImage8.SetResolution(100, 100);
+            overlayImage9 = (Bitmap)Image.FromFile("WKKWimages\\horse9.png");
+            overlayImage9.SetResolution(100, 100);
+            overlayImage10 = (Bitmap)Image.FromFile("WKKWimages\\horse10.png");
+            overlayImage10.SetResolution(100, 100);
+
+            var finalImage = new Bitmap(baseImage.Width, baseImage.Height, PixelFormat.Format32bppArgb);
+            var graphics = Graphics.FromImage(finalImage);
+            graphics.CompositingMode = CompositingMode.SourceOver;
+
+            
+
+            do
+            { //Nieskończona pętla
+                /*
+                for (int i = 1; i < InputPopulation; i++)
+                {
+
+                    int intrandom = random.Next(50, 200);
+                    PictureBox pb = new PictureBox()
+                    {
+                        Size = new Size(30, 30),
+
+                        Location = new Point(5, intrandom),
+                        //BorderStyle = BorderStyle.Fixed3D,
+                        Image = Image.FromFile("C:\\Users\\Galaxis\\Desktop\\WKKWimages\\horse30.png"),
+                        BackColor = Color.Transparent
+                };
+                    lst.Add(pb);
+                    this.Controls.Add(pb);
+                    //pb.BringToFront();
+                    pb.Parent = picture;
+                    pb.BackColor = Color.Transparent;
+
+                } */
+
+
+
+                HorseTest(); //Test
+                System.Windows.Forms.Label x;
+                GlobalFC = 0; //Zerowanie FC <- WYMAGANE
+                SaveBest(); //Zapis najlepszego, o ile lepszy niż to co w pamięci
+                if (ifStop2Arguments(n, InputIteration, getBest(), 28))
+                {
+                    stop = false;
+                    //break; //Sprawdzanie czy stop i przerwanie jeżeli tak
+                    label5.Text = "Ilosc wykonanych iteracji: " + n.ToString();
+                    label6.Text = "Najlepszy osobnik: " + getBest().ToString() + "/30";             
+                }
+                for (int i = 0; i < getPopulationSize(); i++) GlobalFC += getPopulationFC(i); //Sumowanie globalnego FC ze wszystkich koni
+                MinMaxSelector(GlobalFC); //Wyznaczanie min max na podstawie globalnego FC
+                NewGenerationSelector(); //Pierwsza ruletka - losowanie min i max
+                BreedSelector(); //Druga ruletka - losowanie dla 0.5
+                ParentSelector(); //Wyznaczenie listy rodziców
+                CrossBreed(); //Rozmnażanie z rodziców
+                ApplyPopulation(); //Zaakceptowanie nowej populacji (mutacja tutaj też jest)
+                FindBest(); //Sprawdzenie najgorszego z najlepszym z poprzedniej
+                Bests.Add(getBest());
+                            /*std::cout
+                                << "Najlepszy FC: "
+                                << BEST.FC << " w "
+                                << n << " iteracji."
+                                << std::endl; */
+                //label5.Text = n.ToString();
+                n++; //Zwiększanie globalnej iteracji
+            } while (stop);
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            button1.Enabled = true;
+
+            Series series = this.chart1.Series.Add("Best");
+
+
+            for (int i = 0; i < Bests.Count; i++)
+            {
+
+                // Add point.
+                series.Points.Add(Bests[i]);
+                series.ChartType = SeriesChartType.Line;
+                series.BorderWidth = 2;
+
+            }
+
+
+            graphics.DrawImage(baseImage, 0, 0);
+            for (int i = 0; i < getPopulationSize(); i++)
+            {
+                int Jump = getJumpPoints(i);
+                int x;
+                int y;
+
+                if (Jump == 0)
+                {
+                    int randomx = random.Next(323, 352);
+                    int randomy = random.Next(54, 77);
+                    graphics.DrawImage(overlayImage0, randomx, randomy);
+                }
+                if (Jump == 1)
+                {
+                    int randomx = random.Next(202, 250);
+                    int randomy = random.Next(124, 137);
+                    graphics.DrawImage(overlayImage1, randomx, randomy);
+                }
+                if (Jump == 2)
+                {
+                    int randomx = random.Next(195, 225);
+                    int randomy = random.Next(20, 24);
+                    graphics.DrawImage(overlayImage2, randomx, randomy);
+                }
+                if (Jump == 3)
+                {
+                    int randomx = random.Next(393, 393);
+                    int randomy = random.Next(50, 80);
+                    graphics.DrawImage(overlayImage3, randomx, randomy);
+                }
+                if (Jump == 4)
+                {
+                    int randomx = random.Next(205, 253);
+                    int randomy = random.Next(171, 175);
+                    graphics.DrawImage(overlayImage4, randomx, randomy);
+                }
+                if (Jump == 5)
+                {
+                    int randomx = random.Next(80, 114);
+                    int randomy = random.Next(95, 110);
+                    graphics.DrawImage(overlayImage5, randomx, randomy);
+                }
+                if (Jump == 6)
+                {
+                    int randomx = random.Next(0, 5);
+                    int randomy = random.Next(90, 120);
+                    graphics.DrawImage(overlayImage6, randomx, randomy);
+                }
+                if (Jump == 7)
+                {
+                    int randomx = random.Next(16, 60);
+                    int randomy = random.Next(183, 208);
+                    graphics.DrawImage(overlayImage7, randomx, randomy);
+                }
+                if (Jump == 8)
+                {
+                    int randomx = random.Next(284, 305);
+                    int randomy = random.Next(134, 170);
+                    graphics.DrawImage(overlayImage8, randomx, randomy);
+                }
+                if (Jump == 9)
+                {
+                    int randomx = random.Next(198, 250);
+                    int randomy = random.Next(63, 72);
+                    graphics.DrawImage(overlayImage9, randomx, randomy);
+                }
+                if (Jump == 10)
+                {
+                    int randomx = random.Next(59, 136);
+                    int randomy = random.Next(8, 27);
+                    graphics.DrawImage(overlayImage10, randomx, randomy);
+                }
+                //else graphics.DrawImage(overlayImage, 1, 1);
+
+            }
+
+            //show in a winform picturebox
+            //pictureBox1.Image = finalImage;
+
+            //save the final composite image to disk
+            finalImage.Save("WKKWimages\\final.png", ImageFormat.Png);
+
+            var picture = new PictureBox
+            {
+                Name = "skoki",
+                Size = new Size(500, 271),
+                Location = new Point(5, 250),
+                //Image = Image.FromFile("C:\\Users\\Galaxis\\Desktop\\WKKWimages\\final.png"),
+
+            };
+            using (Bitmap bmp = Bitmap.FromFile("WKKWimages\\final.png") as Bitmap)
+            {
+                picture.Image = new Bitmap(bmp);
+            }
+            this.Controls.Add(picture);
+            picture.BringToFront();
+
+
+        }
+
+
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+    }
+}
